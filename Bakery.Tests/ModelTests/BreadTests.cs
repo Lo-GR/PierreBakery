fa0b1fd2301419d7ac800ac9610c58d49c16470c
Lo-GR <diamondintheroth@gmail.com>
2021-02-26 09:46:45 -0800

    write test for ReturnBreadPrice

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -4,5 +4,15 @@ using BreadSpace.Models;
 
 namespace BreadSpace.Tests
 {
-
+  [TestClass]
+  public class BreadTests
+  {
+    [TestMethod]
+    public void ReturnBreadPrice_ReturnIntGivenNumberOfBreads_IntToInt()
+    {
+      int numInput = 5; // 5 Breads
+      int expectedOutput =  Bread.ReturnBreadPrice(5); // 25 dollars
+      Assert.AreEqual(numInput, expectedOutput);
+    }
+  }
 }
