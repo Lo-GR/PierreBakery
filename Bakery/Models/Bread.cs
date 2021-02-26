88e6124d30c77d34ce500285e8702925613d0894
Lo-GR <diamondintheroth@gmail.com>
2021-02-26 10:57:18 -0800

    pass test for ApplyBreadDeals

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -5,13 +5,19 @@ namespace BreadSpace.Models
 {
   public class Bread
   {
+    public static int BasePrice {get; set;}
+    private static int _basePrice = 5;
     public static int ReturnBreadPrice(int numBread)
     {
       return numBread * 5;
     }
     public static int ApplyBreadDeals(int numBreads, int priceBreads)
     {
-      return 5;
+      int returnedPrice = priceBreads;
+      int bOGOBreadAmount = numBreads/2;
+      int bOGODiscount = _basePrice * bOGOBreadAmount;
+      returnedPrice -= bOGODiscount;
+      return returnedPrice;
     }
   }
 }
