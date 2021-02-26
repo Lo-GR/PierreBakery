1a295e04fe19d8256d194c81cef2c2e138921fad
Lo-GR <diamondintheroth@gmail.com>
2021-02-26 12:46:51 -0800

    create test for Pastry Constructor

--- Bakery.Tests/ModelTests/PastryTests.cs
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -1,8 +1,17 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-// using PastrySpace.Models;
+using PastrySpace.Models;
 
-// namespace PastrySpace.Tests
-// {
-
-// }
+namespace PastrySpace.Tests
+{
+  [TestClass]
+  public class PastryTests
+  {
+    [TestMethod]
+    public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
+    {
+      Pastry newPastry = new Pastry(5);
+      Assert.AreEqual(typeof(Pastry), newPastry.GetType());
+    }
+  }
+}
--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -3,5 +3,8 @@ using System.Collections.Generic;
 
 namespace PastrySpace.Models
 {
-
+  public class Pastry
+  {
+    
+  }
 }
