00ad764ba406de224f1fd50e9429c6536677f0aa
Lo-GR <diamondintheroth@gmail.com>
2021-02-26 15:23:13 -0800

    add more UI, create UI method AskBread

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -7,11 +7,22 @@ namespace Bakery
 {
   public class Program
   {
+    public static List<Bread, Pastry> order = 
     public static void Main()
     {
       Console.WriteLine("Welcome to Pierre Bakery!");
       Console.WriteLine("We currently sell BREAD, just the one kind, and PASTRIES, also just one kind.");
-      Console.WriteLine($"The current price for Bread is ");
+      Console.WriteLine($"The current price for Bread is {Bread.ReturnBaseBreadPrice()} dollars each and the current price of Pastry is {Pastry.ReturnBasePastryPrice()} dollars each.");
+      AskBread();
+    }
+    public static void AskBread()
+    {
+      Console.WriteLine("How many Breads would you like? We currently have a buy one get one free sale.");
+      int breadInput = int.Parse(Console.ReadLine());
+      if (breadInput == 0)
+      {
+        Console
+      }
     }
   }
 }
